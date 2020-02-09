b193d720dedc572e82e17784bf0aa840d958fb10
Elvis <elvis.roger369@gmail.com>
2020-02-09 09:07:01 -0300

    adicionando la clase personaa

---
+++ PRUEBA_CORE/Pages/Shared/controller/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRUEBA_CORE.Pages.Shared.controller
+{
+    public class Persona
+    {
+        public int gato { get; set; }
+    }
+}
