bb8a86c0d94fe65b35e834a0aaedfa7b83b4693d
Elvis <elvis.roger369@gmail.com>
2020-02-09 08:50:54 -0300

    pri primer guardado

---
+++ PRUEBA_CORE/Pages/controller/pepe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRUEBA_CORE.Pages.controller
+{
+    public class pepe
+    {
+    }
+}
